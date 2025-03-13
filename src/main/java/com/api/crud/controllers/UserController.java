3e0db6c52f3f826e9306adf54f26e8a22472cecf
Euridice Ingrid <euridiceingrid@gmail.com>
2025-03-13 14:48:40 -0500

    crud users

--- src/main/java/com/api/crud/controllers/UserController.java
+++ src/main/java/com/api/crud/controllers/UserController.java
@@ -1,4 +1,48 @@
 package com.api.crud.controllers;
 
+import com.api.crud.models.UserModel;
+import com.api.crud.services.UserService;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ArrayList<UserModel> getUsers() {
+        return this.userService.getUsers();
+    }
+
+    @PostMapping
+    public UserModel saveUser(@RequestBody UserModel user) {
+        return this.userService.saveUser(user);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<UserModel> getUserById(@PathVariable("id") long id) {
+        return this.userService.getUserById(id);
+    }
+
+    @PutMapping(path = "/{id}")
+    public Optional<UserModel> updateUserById(@PathVariable("id") long id, @RequestBody UserModel user) {
+        return this.userService.updateUserById(id, user);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String deleteUserById(@PathVariable("id") long id) {
+        boolean ok = this.userService.deleteUserById(id);
+        if (ok) {
+            return "User deleted with success";
+        } else {
+            return null;
+        }
+    }
+
 }
--- src/main/java/com/api/crud/services/UserService.java
+++ src/main/java/com/api/crud/services/UserService.java
@@ -1,4 +1,48 @@
 package com.api.crud.services;
 
+import com.api.crud.models.UserModel;
+import com.api.crud.repositories.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
 public class UserService {
+    @Autowired
+    IUserRepository userRepository;
+
+    public ArrayList<UserModel> getUsers() {
+        return (ArrayList<UserModel>) userRepository.findAll();
+    }
+
+    public UserModel saveUser(UserModel user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<UserModel> getUserById(long id) {
+        return userRepository.findById(id);
+    }
+
+    public Optional<UserModel> updateUserById(long id, UserModel request) {
+        Optional<UserModel> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            UserModel userToUpdate = user.get();
+            userToUpdate.setFirstName(request.getFirstName());
+            userToUpdate.setLastName(request.getLastName());
+            userToUpdate.setEmail(request.getEmail());
+            userRepository.save(userToUpdate);
+        }
+        return user;
+    }
+
+    public Boolean deleteUserById(long id) {
+        try {
+            userRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
